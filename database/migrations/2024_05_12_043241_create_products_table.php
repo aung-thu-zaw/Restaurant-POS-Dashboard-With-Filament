bb873027e2f9ba926bdcf2f1a429ab3849080240
Aung Thu Zaw ( Xyder ) <105932994+aung-thu-zaw@users.noreply.github.com>
2024-05-12 11:32:39 +0630

    install filament and create tables for category and product

---
+++ database/migrations/2024_05_12_043241_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->string('image');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('ingredients')->nullable();
+            $table->text('description');
+            $table->integer('qty')->default(0);
+            $table->boolean('is_available')->default(false);
+            $table->decimal('base_price', 8, 2)->default(0);
+            $table->decimal('discount_price', 8, 2)->default(0)->nullable();
+            $table->timestamp('discount_end_time')->nullable();
+            $table->enum('status', ['draft', 'published', 'hidden']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
